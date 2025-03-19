ce29fed4ec0e4cbe0020cd103384190316dd009a
Paul-Evence Coppee <paul-evence@coppee.me>
2025-03-19 18:20:59 +0100

    recipes#index page is in progress

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,17 @@
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
-    @user = current_user
-    @mycomments = Comment.where(user_id: @user.id)
-    @myrecipes = @mycomments.map do |comment|
-      Recipe.find_by(id: comment.recipe_id)
+    @mycomments = Comment.where(user_id: current_user.id)
+    @myrecipes = current_user.recipes
+    @recipes -= @myrecipes
+
+    if params[:query].present?
+      sql_subquery = 'recipes.title ILIKE :query
+        OR recipes.description ILIKE :query
+        OR ingredients.ingredient_name ILIKE :query'
+      @recipes = Recipe.joins(meal_ingredients: :ingredient).where(sql_subquery, query:"%#{params[:query]}%")
+      @myrecipes = @myrecipes.joins(meal_ingredients: :ingredient).where(sql_subquery, query:"%#{params[:query]}%")
+      @recipes -= @myrecipes
     end
   end
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   has_many :meal_ingredients
   has_many :comments
   has_many :menus
+  has_many :ingredients, through: :meal_ingredients
 end
