da520b58cb523c1a1474240ce2f23c8e78f110e3
Paul-Evence Coppee <paul-evence@coppee.me>
2025-03-20 17:00:09 +0100

    empty slots for new meals ready in calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,7 +3,7 @@ class CalendarController < ApplicationController
   def index
     @usercalendar = UserCalendar.find_by(user_id: current_user.id)
     @calendar = @usercalendar.calendar
-    @menus = Menu.where(calendar_id: @calendar.id)
+    @menus = Menu.where(calendar_id: @calendar.id).where('date >= ?', Date.today).order(date: :asc)
   end
 
 end
