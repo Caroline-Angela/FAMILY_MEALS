3bb6a300fffdc4b8fae57b8191cc6e29d8363d7f
Caroline GP <caroline-angela@web.de>
2025-03-21 15:37:53 +0100

    debugging

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,15 +4,4 @@ class Recipe < ApplicationRecord
   has_many :comments
   has_many :menus
   has_many :ingredients, through: :meal_ingredients
-
-  # date flickr
-  validates :opening_date, presence: true
-  validate :future_date
-
-  private
-
-  def future_date
-    return if opening_date.blank?
-    errors.add(:opening_date, "must be in the future") if opening_date < Date.current
-  end
 end
