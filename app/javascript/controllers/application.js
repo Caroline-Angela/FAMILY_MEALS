9fc8bde78ce2d4f7afd235ff76d4dd69c0dfc06f
Caroline-Angela <caroline-angela@web.de>
2025-03-21 15:29:20 +0100

    Update application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -10,14 +10,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-// Connects to data-controller="flatpickr"
-export default class extends Controller {
-  static targets = [ "startTime", "endTime" ]
-  connect() {
-    flatpickr(this.startTimeTarget, {
-              enableTime: true,
-              // Provide an id for the plugin to work
-              plugins: [new rangePlugin({ input: "#end_time"})]})
-    flatpickr(this.endTimeTarget, {})
-  }
-}
