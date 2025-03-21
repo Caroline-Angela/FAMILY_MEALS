0665a967dcfe8858af1d538b39217b597bf625eb
Caroline GP <caroline-angela@web.de>
2025-03-21 14:45:20 +0100

    recipe addition

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,4 +1,7 @@
 import { Application } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+
 
 const application = Application.start()
 
@@ -7,3 +10,14 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = [ "startTime", "endTime" ]
+  connect() {
+    flatpickr(this.startTimeTarget, {
+              enableTime: true,
+              // Provide an id for the plugin to work
+              plugins: [new rangePlugin({ input: "#end_time"})]})
+    flatpickr(this.endTimeTarget, {})
+  }
+}
